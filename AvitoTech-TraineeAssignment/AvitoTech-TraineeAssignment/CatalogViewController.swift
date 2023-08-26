ebaf0d28e8e2c5a0cd490cc9aee06d695cf757fb
Bogdan <bogdan.polygalov@gmail.com>
2023-08-27 00:12:17 +0500

    Создал проект и подготовил к разработке

---
+++ AvitoTech-TraineeAssignment/AvitoTech-TraineeAssignment/CatalogViewController.swift
@@ -0,0 +1,15 @@
+//
+//  CatalogViewController.swift
+//  AvitoTech-TraineeAssignment
+//
+//  Created by Богдан Полыгалов on 26.08.2023.
+//
+
+import UIKit
+
+class CatalogViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+}
---
+++ AvitoTech-TraineeAssignment/AvitoTech-TraineeAssignment/SceneDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  SceneDelegate.swift
+//  AvitoTech-TraineeAssignment
+//
+//  Created by Богдан Полыгалов on 26.08.2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = CatalogViewController()
+        window?.makeKeyAndVisible()
+    }
+}
