28a38333b2f9264cdc500dace57afd2c3f5d27b6
Bogdan <bogdan.polygalov@gmail.com>
2023-08-28 21:55:18 +0500

    Добавил сетевые сервисы для каждого экрана приложения

---
+++ AvitoTech-TraineeAssignment/AvitoTech-TraineeAssignment/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  AvitoTech-TraineeAssignment
+//
+//  Created by Богдан Полыгалов on 28.08.2023.
+//
+
+import Foundation
+
+enum Constants {
+    static let host = "https://www.avito.st/s/interns-ios/"
+}
