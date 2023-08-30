95be3ecd5d16719e62a9271e4a5adf092f58cc94
Bogdan <bogdan.polygalov@gmail.com>
2023-08-30 19:47:18 +0500

    Создал экран католога

--- AvitoTech-TraineeAssignment/AvitoTech-TraineeAssignment/Screens/Catalog/CatalogViewController.swift
+++ AvitoTech-TraineeAssignment/AvitoTech-TraineeAssignment/Screens/Catalog/CatalogViewController.swift
@@ -9,15 +9,41 @@ import UIKit
 
 class CatalogViewController: UIViewController {
     private let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+    private let refreshControl = UIRefreshControl()
+
+    private let catalogNetworkService = CatalogNetworkService.shared
+    private var catalogNetworkServiceObserverData: NSObjectProtocol?
+    private var catalogNetworkServiceObserverError: NSObjectProtocol?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         view.backgroundColor = .systemBackground
         configureCollection()
+        configureRefreshControl()
+
+        catalogNetworkServiceObserverData = NotificationCenter.default
+            .addObserver(
+                forName: CatalogNetworkService.dataReceivedNotification,
+                object: nil,
+                queue: .main
+            ) { [weak self] _ in
+                self?.updateCollection()
+            }
+        catalogNetworkServiceObserverError = NotificationCenter.default
+            .addObserver(
+                forName: CatalogNetworkService.errorNotification,
+                object: nil,
+                queue: .main
+            ) { [weak self] _ in
+                self?.showAlert()
+            }
+        loadData()
     }
 
     private func configureCollection() {
         collectionView.backgroundColor = .clear
+        collectionView.refreshControl = refreshControl
 
         collectionView.dataSource = self
         collectionView.delegate = self
@@ -33,20 +59,53 @@ class CatalogViewController: UIViewController {
             collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
     }
+
+    private func configureRefreshControl() {
+        refreshControl.tintColor = .label
+        refreshControl.addTarget(self, action: #selector(pullToRefresh), for: .valueChanged)
+    }
+
+    private func updateCollection() {
+        UIBlockingProgressHUD.dismiss()
+        collectionView.reloadData()
+    }
+
+    private func showAlert() {
+        UIBlockingProgressHUD.dismiss()
+        let alertTitle = NSLocalizedString("alert.title", comment: "Название алерта")
+        let alertMessage = NSLocalizedString("alert.message", comment: "Сообщение в алерте")
+        let alertActionTitle = NSLocalizedString("alert.action.again", comment: "Название действия повторения в алерте")
+        let alert = UIAlertController(title: alertTitle, message: alertMessage, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: alertActionTitle, style: .default) { [weak self] _ in
+            self?.loadData()
+        })
+        present(alert, animated: true)
+    }
+
+    private func loadData() {
+        UIBlockingProgressHUD.show()
+        catalogNetworkService.fetch()
+    }
+
+    @objc func pullToRefresh() {
+        refreshControl.endRefreshing()
+        loadData()
+    }
 }
 
 extension CatalogViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        // размер ячейки
+        let width = (collectionView.frame.width - 32) / 2 - 10  // MARK: из ширины экрана вычитаем констрейнты (16+16=32) и получаем ширину коллекции, делим её на 2, так как по 2 ячейки, а также вычитаем расстояние между ячейками (10)
+        let height = width / 0.6
+        return CGSize(width: width, height: height)
     }
 
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        // расстояние между ячейками
+        10
     }
 
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        // внутренние отступы
-        // UIEdgeInsets(top: -view.safeAreaInsets.top, left: 0, bottom: 24, right: 0)
+        UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16)
     }
 }
 
@@ -54,16 +113,24 @@ extension CatalogViewController: UICollectionViewDataSource {
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         1
     }
-
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        // количество элементов в секции
+        catalogNetworkService.catalogModel?.advertisements.count ?? 0
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        // создание ячейки
+        let cell: CatalogCell = collectionView.dequeueReusableCell(indexPath: indexPath)
+        guard let model = catalogNetworkService.catalogModel?.advertisements[indexPath.row] else { return cell }
+        cell.id = model.id
+        cell.setImage(link: URL(string: model.imageURL))
+        cell.setNameLabel(name: model.title)
+        cell.setCostLabel(cost: model.price)
+        cell.adressLabel(adress: model.location)
+        cell.dateLabel(date: model.createdDate)
+        return cell
     }
 
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        // открываем экран детальный
+        // TODO: открываем экран детальный
     }
 }
